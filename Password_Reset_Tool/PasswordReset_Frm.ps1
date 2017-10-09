6b25dfb785b725765602a185cc8a44b077277ace
zp33zy <32639172+zp33zy@users.noreply.github.com>
2017-10-09 08:09:19 -0400

    Password-Reset

---
+++ Password_Reset_Tool/PasswordReset_Frm.ps1
@@ -0,0 +1,49 @@
+﻿Import-Module ActiveDirectory
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName PresentationFramework
+$inputXML = @"
+<Window x:Name="PasswordReset_Frm" x:Class="PasswordResetForm.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:PasswordResetForm"
+        mc:Ignorable="d"
+        Title="Password Reset" Height="575" Width="529.5" FontSize="24">
+    <Grid>
+        <TextBox x:Name="Search_Txt" HorizontalAlignment="Left" Height="36" Margin="10,10,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="373" FontSize="26" TextAlignment="Right"/>
+        <Button x:Name="Search_Btn" Content="Search" HorizontalAlignment="Left" Margin="388,10,0,0" VerticalAlignment="Top" Width="123" Height="36" FontSize="24"/>
+        <ListBox x:Name="Users_ListBox" HorizontalAlignment="Left" Height="415" Margin="10,51,0,0" VerticalAlignment="Top" Width="501" FontSize="24"/>
+        <Button x:Name="Unlock_Btn" Content="Unlock" HorizontalAlignment="Left" Margin="10,476,0,10" VerticalAlignment="Center" Width="123" Height="46" FontSize="24"/>
+        <Button x:Name="GenReset_Btn" Content="Generate &amp; Reset" HorizontalAlignment="Left" Margin="138,476,0,10" VerticalAlignment="Center" Width="206" Height="46" FontSize="24"/>
+        <Button x:Name="SpecReset_Btn" Content="Specific Reset" HorizontalAlignment="Left" Margin="349,476,0,10" VerticalAlignment="Center" Width="162" Height="46" FontSize="24"/>
+    </Grid>
+</Window>
+"@
+$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
+
+[void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[xml]$XAML = $inputXML
+#Read XAML
+
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+try{$PasswordReset_Frm = [Windows.Markup.XamlReader]::Load( $reader )}
+catch{Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."}
+
+#===========================================================================
+# Store Form Objects In PowerShell
+#===========================================================================
+
+$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $PasswordReset_Frm.FindName($_.Name)}
+
+Function Get-FormVariables{
+if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay = $true}
+
+
+write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+get-variable WPF*
+}
+
+Get-FormVariables
+$PasswordReset_Frm.ShowDialog() | Out-Null
---
+++ Password_Reset_Tool/Password_Reset_Functions.ps1
@@ -0,0 +1,193 @@
+﻿<#
+	Creator: Zachary C. Preston
+	Date   : 7/10/2017
+	Company: LRC
+#>
+
+function isLegislator([string]$user)
+{
+	$fullName = Get-ADUser -Identity $user | select -expand Name
+	if ($fullName.Contains("(State"))
+	{
+		Write-Host "true"
+		return $true
+	}
+	else
+	{
+		return $false
+	}
+}
+function generateNewPassword([string]$user)
+{
+	
+	$userToReset = $user
+	
+	$newPass = $userToReset[$userToReset.IndexOf('_') + 1]
+	$newPass = $newPass + $userToReset[0]
+	$newPass = $newPass.ToLower()
+	$dayOfWeek = (Get-Date).DayOfWeek.value__
+	switch ($dayOfWeek)
+	{
+		1
+		{
+			$newPass += "!";
+			break
+		}
+		2
+		{
+			$newPass += "@";
+			break
+		}
+		3
+		{
+			$newPass += "#";
+			break
+		}
+		4
+		{
+			$newPass += "`$";
+			break
+		}
+		5
+		{
+			$newPass += "%";
+			break
+		}
+		default
+		{
+			$newPass += "!";
+			break
+		}
+	}
+	if (isLegislator -user $user)
+	{
+		$UserOU = "legi"
+	}
+	else
+	{
+		[string]$distinguishedName = Get-ADUser -Identity $user | select -expand DistinguishedName
+		[string[]]$OUs = $distinguishedName.Split("=")
+		$UserOU = $OUs[3].Substring(0, $OUs[3].IndexOf(","))
+		if ($UserOU.Length -le 3)
+		{
+			$UserOU = $UserOU.Substring(0, 3) + "1"
+		}
+		else
+		{
+			$UserOU = $UserOU.Substring(0, 4)
+		}
+		if ($UserOU.Equals("STAT") -or $UserOU.Equals("Stat"))
+		{
+			$UserOU = "sgov"
+		}
+		$UserOU = $UserOU.ToLower()
+	}
+	$newPass += $UserOU
+	#$minute = 3
+	$minute = (Get-Date).Minute
+	if ($minute -le 9)
+	{
+		$newPass += "0" + $minute
+	}
+	else
+	{
+		$newPass += $minute
+	}
+	return $newPass
+}
+function resetADPassword ([string]$username)
+{
+	Add-Type -AssemblyName System.Windows.Forms
+	Add-Type -AssemblyName System.Drawing
+	Add-Type -AssemblyName PresentationFramework
+	
+	[string]$newPassword = generateNewPassword -user $username
+	$securePass = ConvertTo-SecureString -String $newPassword -AsPlainText -Force
+	$displayName = Get-ADUser $username | select -expand Name
+	#$confirmYesNo = [System.Windows.MessageBox]::Show("Reset Password To $newPassword" + "?", "Password Reset For $displayName", 'YesNo', 'Error')
+	$confirmBox = New-Object -ComObject Wscript.Shell
+	$answer = $confirmBox.popup("Reset " + $username + "'s password to: " + $newPassword + "?", 0, "Reset User Password: " + $username, 4)
+	if ($answer -eq 6)
+	{
+		$secondAnswer = $confirmBox.popup("Commit Changes to $($displayName)?", 0, $displayName + "'s new password: " + $newPassword, 4)
+		if ($secondAnswer -eq 6)
+		{
+			try
+			{
+				Set-ADAccountPassword $username -NewPassword $securePass -Reset
+				unlockAccount -username $username
+				$clipboardBiulder = "The Password For The Account: "
+				$clipboardBiulder += "$displayName has been reset to: `r`n`t"
+				$clipboardBiulder += "$newPassword `r`nThis can be changed after two days.`r`nThank You"
+				Set-Clipboard -Value $clipboardBiulder
+				$confirmBox.popup("Changes Complete, Results Copied To Clipboard!", 0, "Applied!")
+				#$outputBox.AppendText("The password for the account: ")
+				return $newPassword
+				
+			}
+			catch
+			{
+				$confirmBox.popup("Error: Could Not Perform Action!")
+			}
+		}
+		else
+		{
+			$confirmBox.popup("No Changes Made")
+		}
+	}
+	else
+	{
+		$confirmBox.popup("No Changes Made")
+	}
+	
+}
+function resetADPasswordSpecific ([string]$username, [string]$specificPass)
+{
+	Add-Type -AssemblyName System.Windows.Forms
+	Add-Type -AssemblyName System.Drawing
+	Add-Type -AssemblyName PresentationFramework
+	
+	$newPassword = $specificPass
+	$securePass = ConvertTo-SecureString -String $newPassword -AsPlainText -Force
+	$displayName = Get-ADUser $username | select -expand Name
+	$confirmYesNo = [System.Windows.MessageBox]::Show("Reset Password To $newPassword" + "?", "Password Reset For $displayName", 'YesNo', 'Error')
+	$confirmBox = New-Object -ComObject Wscript.Shell
+	$answer = $confirmBox.popup("Reset " + $username + "'s password to: " + $newPassword + "?", 0, "Reset User Password: " + $username, 4)
+	if ($answer -eq 6)
+	{
+		$secondAnswer = $confirmBox.popup("Commit Changes to $($displayName)?", 0, $displayName + "'s new password: " + $newPassword, 4)
+		if ($secondAnswer -eq 6)
+		{
+			try
+			{
+				Set-ADAccountPassword $username -NewPassword $securePass -Reset
+				unlockAccount -username $username
+				$clipboardBiulder = "The Password For The Account: "
+				$clipboardBiulder += "$displayName has been reset to: `r`n`t"
+				$clipboardBiulder += "$newPassword `r`nThis can be changed after two days.`r`nThank You"
+				Set-Clipboard -Value $clipboardBiulder
+				$confirmBox.popup("Changes Complete, Results Copied To Clipboard!", 0, "Applied!")
+				#$outputBox.AppendText("The password for the account: ")
+				
+				Write-Host "$displayName $newPassword"
+			}
+			catch
+			{
+				$confirmBox.popup("Error: Could Not Perform Action!")
+			}
+		}
+		else
+		{
+			$confirmBox.popup("No Changes Made")
+		}
+	}
+	else
+	{
+		$confirmBox.popup("No Changes Made")
+	}
+	
+}
+function unlockAccount([string]$username)
+{
+	Unlock-ADAccount -Identity $username
+}
